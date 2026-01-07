05f0ef61568492d157d2972d634fc3f4b8d09e35
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-07 22:31:38 +0530

    Added simplified searchForm with react widgets

--- Estate-Agent-WebApplication/src/Components/SearchForm.jsx
+++ Estate-Agent-WebApplication/src/Components/SearchForm.jsx
@@ -19,7 +19,54 @@ export function SearchForm({onSearch}){
             [name]:value,
 
     }));
+    const handleSubmit=(e)=>{
+        e.preventDefault();//will not refresh the web page when submitting.
+        onSerach(criteria);
+
+    };
+    return(
+        <form onsubmit={handleSubmit}>
+        <label>Property Type</label>
+        <DropdownList
+            data={['any','house','flat']}
+            value={criteria.type}{/*currently selected value.*/}
+            onChange={val => handleChange('type',val)
+            }
+        />
+        {/*Prices*/ }
+        <label>Price</label>
+        <NumberPicker
+            value={criteria.price}
+            onChange={val =>handleChange('price',val)}
+            min={0}//from this user cannot pick a negative value.input validation
+
+            
+        />
+        <label>Bedrooms:</label>
+        <NumberPicker
+            value={criteria.bedrooms}
+            onChange={val => handleChange('bedrooms', val)}
+            min={0}
+      />
+        <label>Available Date:</label>
+        <DateTimePicker
+            value={criteria.date}
+            onChange={val => handleChange('date', val)}
+      />
+
+        <label>Postcode:</label>
+        <input
+            type="text"
+            value={criteria.postcode}
+            onChange={e => handleChange('postcode', e.target.value)}
+      />
+
+      <button type="submit">Search</button>
+
+         </form>
+    )
     }
+
     
 
 }
