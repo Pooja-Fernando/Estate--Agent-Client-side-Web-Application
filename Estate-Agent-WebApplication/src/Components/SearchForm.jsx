598ec5bafb9ec80c95039db02534a20e38913496
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-07 19:46:18 +0530

    added the functional component 'SearchForm and initialized all search fields'

--- Estate-Agent-WebApplication/src/Components/SearchForm.jsx
+++ Estate-Agent-WebApplication/src/Components/SearchForm.jsx
@@ -1,1 +1,16 @@
- import './SearchForm.css';
+import {useState} from 'react';
+import './SearchForm.css';
+export function SearchForm({onSearch}){
+    const[criteria,setCriteria]=useState({
+        type: 'any',
+        minPrice:'',
+        maxPrice:'',
+        minBedrooms:'',
+        maxBedrooms:'',
+        dateStart:'',
+        postcodeAra:'',
+
+    });
+    
+
+}
