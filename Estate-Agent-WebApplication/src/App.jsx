38b4683b18ca94b6d71cb5c38e2d09055d487477
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-08 18:37:00 +0530

    create the App.jsx set up routing and provides the global favourites state to the app.

--- Estate-Agent-WebApplication/src/App.jsx
+++ Estate-Agent-WebApplication/src/App.jsx
@@ -1,19 +1,41 @@
-import SearchForm from './Components/SearchForm';
-import PropertiesData from '.data/properties.json';
-import './App.css'
-import FavouritesProvider from './Context/FavouriteContext';
+
+import {BrowserRouter,Route} from 'react-router';
+import { FavouritesProvider } from './Context/FavouriteContext';
+
+//importing pages
+import SearchPage from './pages/searchPage';
+import PropertyPage from './pages/propertyPage';
+
+
+import './App.css';
 
 function App() {
-  <FavouritesProvider>
-  </FavouritesProvider>
-
-
+  
 
   return (
-    <SearchForm/>
+   < BrowserRouter>
+     <FavouritesProvider>
+      <header>
+        <h1>
+          Estate Client Agent
+        </h1>
+      </header>
+      <BrowserRouter>
+        <Route path ='/' element= {<SearchPage/>}/>
+        <Route path ="/property/:id" element={<PropertyPage/>}/> 
+        <Route path="*" element={<h1>404: Page Not Found</h1>} />
+      </BrowserRouter>
+      <footer>
+          <p>&copy; 2024 Estate Client App</p>
+      </footer>
+
+      </FavouritesProvider>
+      
+    </BrowserRouter>
+    
     
 
-  )
+  );
 }
 
 export default App
