325d9e5c395753b1478dd3d83c813c409be9e692
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-08 15:57:31 +0530

    Handle the drop

--- Estate-Agent-WebApplication/src/Components/FavouriteList.jsx
+++ Estate-Agent-WebApplication/src/Components/FavouriteList.jsx
@@ -1,8 +1,35 @@
 import {FavouritesContext} from '../Context/FavouriteContext';
 
-@param {object[]} allproperties 
+@param{object[]}lproperties
+
 function FavouritesList({allproperties}){
     // access the state and handles from the favourites context
-    const {favourites ,addFavourites, removeFavourites,  clearFavourites}= useContext(favouritesContext);
+    const {favourites ,addFavourites, removeFavourites,  clearFavourites}= useContext(FavouritesContext);
+    //allows dropping
+    const handledragOver=(e)=>{
+        e.preventDefault();
+    };
+
+    //Handle the drop.
+    const handleDropOver = (e)=>{
+        e.preventDefault();
+    };
+    
+    const handleDrop=(e) => {
+        e.preventDefault();
+        //get the property id that was saved when dragging started
+        const propertyIdString = e.dataTransfer.getData("propertyId");
+        if (propertyIdString){
+            const propertyId = Number(propertyIdString);
+
+
+
+            const propertyToAdd= allproperties.find(p=>p.id===propertyId);
+            
+            if (propertyToAdd){
+                addFavourites(propertyToAdd);//add to favs
+            }
+        }
+    }
 
 }
