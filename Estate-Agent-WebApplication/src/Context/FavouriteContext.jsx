07900bd6537ce333c85ea1b0f17c1e5babdad063
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-08 13:55:08 +0530

    Added a function to remove the property.

--- Estate-Agent-WebApplication/src/Context/FavouriteContext.jsx
+++ Estate-Agent-WebApplication/src/Context/FavouriteContext.jsx
@@ -6,15 +6,20 @@ const favouritesProvider=({children})=>{
     
     //function to add a property and ensuring no duplicates.
     const addFavourite=useCallBack((property)
-        setFavourites(previous=>){
+        setFavourites(previous=>{
             const isDuplicate=previous.some(fav =>fav.id === property.id);
             if (!isDuplicate){
                 return[...previous, property];
             }
             return previous;
         });
+        []);
+        //function to remove a property by its id 
+        const removefavourite =useCallback((propertyId) => {
+            setFavourite =>(previous.filter(fav.id !== propertyId));
+        }, []);
 
-    }  , []);
+    };
+    
     const 
 
-}
