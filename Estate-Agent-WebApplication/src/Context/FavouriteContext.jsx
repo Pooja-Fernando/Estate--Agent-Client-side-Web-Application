39cdf7e27986ae91350625b8b4ae57e8e4b7c649
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-08 11:51:32 +0530

    Added the provider component and imported the useCallback function.

--- Estate-Agent-WebApplication/src/Context/FavouriteContext.jsx
+++ Estate-Agent-WebApplication/src/Context/FavouriteContext.jsx
@@ -1,2 +1,20 @@
 import {createContext} from 'react';
+import {useCallback} from 'react';
 const favouritesContext =createContext(null);
+const favouritesProvider=({children})=>{
+    const[favourites, setFavourites]=useState[()];
+    
+    //function to add a property and ensuring no duplicates.
+    const addFavourite=useCallBack((property)
+        setFavourites(previous=>){
+            const isDuplicate=previous.some(fav =>fav.id === property.id);
+            if (!isDuplicate){
+                return[...previous, property];
+            }
+            return previous;
+        });
+
+    }  , []);
+    const 
+
+}
