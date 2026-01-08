1d2c475ba3b37bcd3b277b673d3bbc6148c02a8e
Pooja-Fernando <poojafernando16@gmail.com>
2026-01-08 16:53:29 +0530

    Add imports

--- Estate-Agent-WebApplication/src/pages/propertyPage.jsx
+++ Estate-Agent-WebApplication/src/pages/propertyPage.jsx
@@ -1,4 +1,6 @@
 import React ,{useState} from 'react';
+import {Link} from 'react-router-dom';
+import All_Properties from './data/properties.json';'
 export function PropertyImage({images}){
     const[mainImage,setMainImage]=useState(images[0]);//settimg the first image as default.
     
